31dcc957c1d32915b5792a4dc2b7b1e430c692c1
Octavian Vaideanu <octavianvaideanu@Octavians-MacBook-Pro.local>
2019-01-25 14:22:56 +0200

    just for test

--- client/components/common/pageTitle.js
+++ client/components/common/pageTitle.js
@@ -1,6 +1,6 @@
 const PageTitle = (props) => (
   <div className="row">
-    <div className="col-md">
+    <div className="col-md ">
       <div className="jumbotron">
         <h1 className="text-center">{props.pageTitle}</h1>
       </div>
